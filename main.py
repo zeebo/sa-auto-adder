a7438a9cf87fd4407863da30653d55817cc8037e
Zeebo <leterip@gmail.com>
2009-10-10 03:14:22 -0400

    got django with better appengine support going

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
-# main.py
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import os, sys
-os.environ["DJANGO_SETTINGS_MODULE"] = "sa_auto_adder.settings"
-sys.path.append("/Users/zeebo/Desktop/sa_auto_adder")
+"""Bootstrap for running a Django app under Google App Engine.
+
+The site-specific code is all in other files: settings.py, urls.py,
+models.py, views.py.  And in fact, only 'settings' is referenced here
+directly -- everything else is controlled from there.
+
+"""
+
+# Standard Python imports.
+import os
+import sys
+import logging
+
+from appengine_django import InstallAppengineHelperForDjango
+InstallAppengineHelperForDjango()
+
+from appengine_django import have_django_zip
+from appengine_django import django_zip_path
 
 # Google App Engine imports.
 from google.appengine.ext.webapp import util
 
-# Force Django to reload its settings.
-from django.conf import settings
-settings._target = None
-
+# Import the part of Django that we use here.
 import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
-
-# Log errors.
-#django.dispatch.dispatcher.connect(
-#   log_exception, django.core.signals.got_request_exception)
-
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(django.db._rollback_on_exception,django.core.signals.got_request_exception)
 
 def main():
-    # Create a Django application for WSGI.
-    application = django.core.handlers.wsgi.WSGIHandler()
+  # Ensure the Django zipfile is in the path if required.
+  if have_django_zip and django_zip_path not in sys.path:
+    sys.path.insert(1, django_zip_path)
 
-    # Run the WSGI CGI handler with that application.
-    util.run_wsgi_app(application)
+  # Create a Django application for WSGI.
+  application = django.core.handlers.wsgi.WSGIHandler()
 
-if __name__ == "__main__":
-    main()
+  # Run the WSGI CGI handler with that application.
+  util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
--- robot.py
+++ robot.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-import smilies
 from waveapi import events
 from waveapi import model
 from waveapi import robot
 from waveapi import document
 
 if __name__ == '__main__':
-  myRobot = robot.Robot('sa-emote', 
+  myRobot = robot.Robot('sa-auto-adder', 
       image_url='http://sa-auto-adder.appspot.com/assets/icon.png',
       version='1',
       profile_url='http://sa-auto-adder.appspot.com/')
-  myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
   myRobot.Run()

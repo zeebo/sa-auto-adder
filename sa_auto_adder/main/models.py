ee93b72f91fd96e5337dbc6e8aebe25aca2768e3
Zeebo <leterip@gmail.com>
2009-10-10 03:00:16 -0400

    removing old django code

--- sa_auto_adder/main/models.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import db
-
-class User(db.Model):
-  username = db.StringProperty(required=True)
-  password = db.StringProperty(required=True)
-  reg_date = db.DateTimeProperty(auto_now_add=True)
